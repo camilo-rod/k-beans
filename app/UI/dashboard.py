277ca2687ded463fdbe03e83094909035e062ad9
camilo-rod <camiloandres1213@gmail.com>
2026-05-28 16:53:15 -0500

    texto

--- app/UI/dashboard.py
+++ app/UI/dashboard.py
@@ -11,6 +11,5 @@ def render(kmeans, df):
     c3.metric("Registros", len(df))
 
     st.markdown(
-        "K-Beans utiliza Machine Learning para detectar patrones geométricos "
-        "y clasificar frijoles según sus características físicas."
+        "K-Beans utiliza K-Means para detectar patrones geométricos y clasificar frijoles según sus características físicas."
     )
